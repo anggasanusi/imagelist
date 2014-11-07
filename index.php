52ec0477f35364ebf170c5d5c01f0d43a58b36a5
anggasanusi <xsuck91@gmail.com>
2014-11-07 08:02:42 +0700

    hilangkan fungsi default

--- index.php
+++ index.php
@@ -3,20 +3,11 @@ include "config.php";
 error_reporting(E_ALL ^ E_NOTICE);
 $id = $_GET['page'];
 $row = array();
-if($id == '')
-{
-$data = mysql_query("SELECT id, user_username, category, title, picurl, timeup, gvote, bvote FROM media ORDER BY id ASC LIMIT 0,3");
-while($result = mysql_fetch_assoc($data)){
-$row[] = $result;
-}
-echo "{\"post\":".json_encode($row). "}";
-}
-else
-{
+
 $data = mysql_query("SELECT id, user_username, category, title, picurl, timeup, gvote, bvote FROM media ORDER BY id ASC LIMIT $id, 3");
 while($result = mysql_fetch_assoc($data)){
 $row[] = $result;
 }
 echo "{\"post\":".json_encode($row). "}";
-}
+
 ?>
